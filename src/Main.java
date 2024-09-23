3904cb0bea93100a7bd6fd3bd860ec79eb14268d
Dale Hendricks <dalerhendricks@outlook.com>
2024-09-23 13:20:47 -0700

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    final String BOT_TOKEN = "";
+    JDA api = JDABuilder.createDefault(BOT_TOKEN).build();
 }
