39829cb4c1788f67a03c631ef06bf6c4332b4622
Dale Hendricks <dalerhendricks@outlook.com>
2024-09-23 18:15:14 -0700

    please

---
+++ src/EchoListener.java
@@ -0,0 +1,10 @@
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class EchoListener extends ListenerAdapter {
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        if (event.getName().equals("echo") && event.getChannel().getName().contains("bot")) {
+            event.reply(event.getOption("message").getAsString()).queue(); // reply immediately
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
 
 public class Main {
-    final String BOT_TOKEN = "";
-    JDA api = JDABuilder.createDefault(BOT_TOKEN).build();
+    public static void main(String[] args) throws InterruptedException {
+        final String BOT_TOKEN = System.getenv("BOT_TOKEN");
+        JDA api = JDABuilder.createDefault(BOT_TOKEN).build();
+        Guild guild = api.awaitReady().getGuildsByName("VIP Brogrammers", true).get(0);
+        System.out.println(guild.getName());
+        api.addEventListener(new EchoListener());
+        guild.updateCommands().addCommands(
+                Commands.slash("echo", "Echoes the provided input.")
+                        .addOption(OptionType.STRING, "message", "The message to repeat.", true)
+        ).queue();
+    }
 }
